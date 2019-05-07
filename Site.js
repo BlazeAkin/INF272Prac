22fbd23c56e23d9c50c29664218a9b9416b41915
Gideon Botha <46783852+BlazeAkin@users.noreply.github.com>
2019-05-07 11:03:09 +0200

    Update Site.js

---
+++ Site.js
@@ -0,0 +1,3 @@
+function ShowName() {
+window.alert("I am excited for 354 because of all the new coding knowledge we will be dabbling with. And things that are relevant in today's business and IT environments")
+}

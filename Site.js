2becb6b80fe0bade3d55adeba0b6605428f155bb
le-ginger <48588630+le-ginger@users.noreply.github.com>
2019-05-07 11:11:07 +0200

    Update Site.js

--- Site.js
+++ Site.js
@@ -1,3 +1,3 @@
 function aiden_click() {
-  alert('I have absolutely no idea what we are gonna do in INF 354 :-(');
+  window.alert('I have absolutely no idea what we are gonna do in INF 354 :-(');
 }
